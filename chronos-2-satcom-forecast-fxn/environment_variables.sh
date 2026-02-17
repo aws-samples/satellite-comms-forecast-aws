fc5925e86b77db66352b0225e032135ed68bec6f
alancaws <alanwc@amazon.com>
2026-02-17 22:33:30 +0000

    Updated README

--- chronos-2-satcom-forecast-fxn/environment_variables.sh
+++ chronos-2-satcom-forecast-fxn/environment_variables.sh
@@ -1,4 +1,7 @@
-export ENDPOINT_NAME="jumpstart-dft-pt-forecasting-chrono-20260216-185712"
+#export ENDPOINT_NAME="jumpstart-dft-pt-forecasting-chrono-20260216-185712"
+export ENDPOINT_NAME="pt-forecasting-chronos-2-2026-02-17-22-11-41-902"
+
 #export LAMBDA_FUNCTION_NAME="chronos-2-satcom-forecast-lambda"
 export LAMBDA_FUNCTION_NAME="satcom-chronos-forecast"
+
 export AWS_REGION="us-west-2"
